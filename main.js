fabd1b20770bf9c27b15861437672967b33132a6
NJ <souza.junior.nelson@escola.pr.gov.br>
2025-06-11 11:00:40 -0300

    asdas

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const menuToggle = document.getElementById("menu-toggle");
-const sideMenu = document.getElementById("side-menu");
-const closeMenu = document.getElementById("close-menu");
+// Seleciona os elementos
+const menuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+const closeMenu = document.getElementById('close-menu');
 
-menuToggle.addEventListener("click", () => {
-  sideMenu.classList.add("show");
+// Abre o menu ao clicar no botão ☰
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.remove('hidden');
 });
 
-closeMenu.addEventListener("click", () => {
-  sideMenu.classList.remove("show");
+// Fecha o menu ao clicar no botão ×
+closeMenu.addEventListener('click', () => {
+  sideMenu.classList.add('hidden');
 });
